860617c62980a977a0ead7e1cf46647b0c186e1d
Juan José Fernández Moreno <jjmateo1279@gmail.com>
2023-10-01 23:25:40 +0200

    Basic estructure
    create project's basic structure

--- Solitaire/views/Game.xaml.cs
+++ Solitaire/views/Game.xaml.cs
@@ -10,15 +10,14 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Solitaire {
+namespace Solitaire.views {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica de interacción para Game.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow( ) {
+    public partial class Game : Window {
+        public Game( ) {
             InitializeComponent( );
         }
     }
